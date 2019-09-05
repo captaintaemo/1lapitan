6d5bdb96f1a8151a3fe11da27e61cc55b7be87b6
Christian lapitan <user name@LAPTOP-29M0O7H1>
2019-09-05 20:34:06 +0800

    Lapitan Activity 1

---
+++ src/Configurator/Connector.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Configurator;
+
+/**
+ *
+ * @author user name
+ */
+public class Connector {
+ public static String conURL = "jdbc:mysql://localhost/dbmslapitan21?"
+                    + "user=root&password=";    
+}
